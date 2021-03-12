b9be1e4b8ff20086823167d9cc0763e0935aca0b
ppigmon <76710496+ppigmon@users.noreply.github.com>
2021-03-11 20:48:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*goal of this JS. 
+step 1: have user input text into the text field
+step 2: when user is done they will click the submit button
+step 3: when submit button is clicked, text will populate below in a list item(li)
+step 4: when list item is created it will have two options, 1) option to delete item 2) option to show item is completed.  */
+
+
+
+
